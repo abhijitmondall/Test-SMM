5ae3bbb144e02dfef4478d542dceb82d8424cfca
Abhijit Mondal <abhijitmondal.cse@gmail.com>
2025-03-02 14:19:09 +0600

    Code Refactor

--- Server-Side/routes/userRoutes.js
+++ Server-Side/routes/userRoutes.js
@@ -5,6 +5,6 @@ const router = express.Router();
 // router.post("/signup", authController.signup);
 router.get("/login", authController.login);
 
-router.route("/callback").get(authController.callback);
+router.route("/callback").post(authController.callback);
 
 module.exports = router;
